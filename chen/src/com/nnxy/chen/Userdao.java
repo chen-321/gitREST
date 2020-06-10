d1a82690dbee068e7ea79a08bd99328049b27f29
chen-321 <1948921956@qq.com>
2020-06-10 11:04:20 +0800

    对用户模块进行DDL语句的操作

---
+++ chen/src/com/nnxy/chen/Userdao.java
@@ -0,0 +1,20 @@
+package com.nnxy.chen;
+
+import java.util.List;
+
+public class Userdao {
+
+	public void addUser(int id,String username,String password) {
+		
+	}
+	public void deleteUser(int id) {
+		
+	}
+	public void updateUser(int id,String newUsername,String newPassword) {
+		
+	}
+	public List<Object>getUsers(int pageIndex,int pageSize){
+	
+		return null;
+	}
+}
